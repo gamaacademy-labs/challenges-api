5fc379296214180d343cce3e07321ed0f659da48
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2022-08-13 15:26:24 -0300

    feat: adding error message

--- src/domain/userChallenges/userChallenges.service.ts
+++ src/domain/userChallenges/userChallenges.service.ts
@@ -1,9 +1,17 @@
 import sequelize from "sequelize";
+import ChallengesModel from "../challenges/challenges.model";
 import { UserChallenge } from "./userChallenges.entity";
 import UserChallengesModel from "./userChallenges.model";
 
 const UserChallengesService = {
   async getScoresByChallenge(challengeId: string): Promise<UserChallenge[]> {
+    const challengeExists = await ChallengesModel.count({
+      where: {
+        id: challengeId
+      }
+    });
+    if (!challengeExists) throw new Error("Desafio não encontrado");
+
     const usersScores = await UserChallengesModel.findAll({
       where: {
         challengeId
@@ -11,6 +19,7 @@ const UserChallengesService = {
       include: "user",
       order: [["score", "DESC"]]
     });
+
     return usersScores as unknown as UserChallenge[];
   }
 };
