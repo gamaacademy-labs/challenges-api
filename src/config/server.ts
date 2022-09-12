f7cc276d8ceae4dedfbfd029a0a36f1729ba924b
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2022-09-12 14:56:33 -0300

    feat: adding swagger

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,11 +1,14 @@
-import express from "express";
-import routes from "../routes";
 import cors from "cors";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import routes from "../routes";
+import swaggerDocs from "../swagger.json";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use("/v1", routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 export default app;
